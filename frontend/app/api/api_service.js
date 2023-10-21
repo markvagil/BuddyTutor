ac92b6f0999932e58a6fa1c54f22fd082aefa1fa
dosmaster4life <jefferysuperman@gmail.com>
2023-10-21 05:54:12 -0600

    updated api and prompt engineered for history

--- frontend/app/api/api_service.js
+++ frontend/app/api/api_service.js
@@ -97,24 +97,29 @@ export function getAnalytics(CourseId, AssignmentId) {
   "assignmentId": "A1",
   "studentId": "joey",
   "question": "hello, what is 100 * 100"*/
-
-export function addQuestion(CourseId = "CS101", AssignmentId = "A1", StudentId = "joey", Question = "Hello, help me please.") {
-  return fetch(`${API_URL}add_question`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ CourseId, AssignmentId, StudentId, Question }),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response;
+  export function addQuestion(CourseId = "CS101", AssignmentId = "A1", StudentId = "joey", Question = "Hello, help me please.",  messagesJSON) {
+    return fetch(`${API_URL}add_question`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        courseId: CourseId,
+        assignmentId: AssignmentId,
+        studentId: StudentId,
+        question: Question,
+        messagesJSON: messagesJSON,
+      })
     })
-    .catch((error) => {
-      console.error("Error:", error);
-      throw error; // propagate the error so that it can be caught in the calling function
-    });
-}
-
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        throw error;
+      });
+  }
+  
--- frontend/app/chat/page.jsx
+++ frontend/app/chat/page.jsx
@@ -1,67 +1,61 @@
 "use client";
 import React, { useState } from "react";
-
-// Dummy API_URL
-const API_URL = "http://localhost:3000/";
-
-export function addQuestion(CourseId = "CS101", AssignmentId = "A1", StudentId = "joey", Question = "Hello, help me please.") {
-  return fetch(`${API_URL}add_question`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ CourseId, AssignmentId, StudentId, Question }),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response;
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      throw error;
-    });
-}
+import { addQuestion } from "../api/api_service"; // Make sure the addQuestion function is correctly imported
 
 export const Chat = () => {
   const [messages, setMessages] = useState([]);
   const [currentMessage, setCurrentMessage] = useState("");
+  const [loading, setLoading] = useState(false); // State to track loading
+
+  const addMessage = (type, content) => {
+    setMessages((prevMessages) => [...prevMessages, { type, content }]);
+  };
 
   const sendMessage = async () => {
+    setLoading(true); // Set loading state to true before API call
     try {
-      // Add the new user message to the chat
-      setMessages([...messages, { type: "user", content: currentMessage }]);
+      addMessage("user", currentMessage);
+      console.log(`Most recent message: ${currentMessage}`);  // Log the most recent message
 
-      // Call addQuestion function to send the question
-      await addQuestion("CS101", "A1", "joey", currentMessage);
+      // Convert the entire chat history to a JSON-formatted string
+      const messagesJSON = JSON.stringify(messages);
 
-      // Simulate a bot response (In a real application, this should be the response from the server)
-      setMessages([...messages, { type: "user", content: currentMessage }, { type: "bot", content: "How can I assist you?" }]);
+      // Call addQuestion function to send the question and the entire chat history
+      const response = await addQuestion("CS101", "A1", "joey", currentMessage, messagesJSON); // Adding messagesJSON as a parameter
 
+      // Parse the JSON response to get the actual message content
+      const responseData = await response.json();
+
+      addMessage("bot", responseData.response || "How can I assist you?");
+      
       // Clear the input field
       setCurrentMessage("");
     } catch (error) {
       console.error("Failed to send message:", error);
+      addMessage("bot", "Error sending message.");
+    } finally {
+      setLoading(false); // Set loading state to false after API call
     }
   };
-
   return (
-    <main className="">
-      <div className="chat-window">
+    <main className="bg-gray-100 p-4">
+      <div className="chat-window bg-white rounded-lg p-4">
         {messages.map((message, index) => (
-          <div className={`message ${message.type}`} key={index}>
+          <div className={`message ${message.type === "user" ? "text-blue-500" : "text-green-500"} mb-2`} key={index}>
             {message.content}
           </div>
         ))}
       </div>
-      <div className="chat-input">
+      <div className="chat-input mt-4 flex items-center">
         <input
+          className="rounded-lg border-2 border-gray-300 flex-1 mr-2 p-2"
           type="text"
           value={currentMessage}
           onChange={(e) => setCurrentMessage(e.target.value)}
         />
-        <button onClick={sendMessage}>Send</button>
+        <button className="bg-blue-500 text-white p-2 rounded-lg" onClick={sendMessage} disabled={loading}>
+          {loading ? "Loading..." : "Send"}
+        </button>
       </div>
     </main>
   );

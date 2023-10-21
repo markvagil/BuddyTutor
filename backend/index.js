b391a850774ce5840ec5cc659fab0af21e4e0e3f
dosmaster4life <jefferysuperman@gmail.com>
2023-10-21 15:47:09 -0600

    fixes for serving

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ require("dotenv").config();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://frontend-dxle.onrender.com"],
     credentials: false,
   })
 );
--- frontend/app/api/api_service.js
+++ frontend/app/api/api_service.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/";
+const API_URL = "https://buddy-tutor.onrender.com/";
 
 export function getCourses() {
   return fetch(`${API_URL}allCourses`, {

8e50c5513438fdd568d6ff20c817e576420b492b
dosmaster4life <jefferysuperman@gmail.com>
2023-10-20 18:46:29 -0600

    Backend Auth0 implementation

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+// Express app
+
+const express = require('express');
+const { auth } = require('express-openid-connect');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// enviorment variables
+require('dotenv').config();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+app.use(auth(config));
+
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  });
+
+
+app.listen(port, () => console.log(`Backend listening on ${port}!`));
